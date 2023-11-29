2f96e4fc5ca40da7f7638a4159f93be1adfdaa1b
karan chokshi <karansoni0810@gmail.com>
2023-11-29 20:44:19 +0000

    set font family and space and margin

--- js/main.js
+++ js/main.js
@@ -208,8 +208,7 @@
 	  $(function() {
 		$(window).on("scroll", function() {
 			
-			if($(window).scrollTop() > 730) {
-				console.log(".........")
+			if($(window).scrollTop() > 710) {
 				$(".kc_main_navigations").addClass("active");
 			} else {
 				//remove the background property so it comes transparent again (defined in your css)
